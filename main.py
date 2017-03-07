412196aacc6bcb1924fc982a6b160005a0ec4d61
Simon Ko <eumon4@berkeley.edu>
2017-03-06 21:40:39 -0800

    added more to all three classes. have a better idea of how this is going to go now. Main parts of this commit are the update function got a lot more update-y and the width is now one global variable that is referenced rather than having a bunch of width variables everywhere.

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
 import pygame, sys
 from pygame.locals import *
+import snake
+import food
 
 pygame.init()
 FPS = 30 
 fpsClock = pygame.time.Clock()
 
 #setting up the window
-DISPLAYSURF = pygame.display.set_mode((400,300)) #sets width and heigh of window to 400,300 respectively
+XLEN = 400
+YLEN = 400
+DISPLAYSURF = pygame.display.set_mode((XLEN, YLEN))  #sets width and heigh of window to 400,300 respectively
 pygame.display.set_caption('It\'s A Snake! ...Game.')
+WIDTH = 20
 
 #set up colors
-BLACK = (0,0,0) #background
-WHITE = (255,255,255) #snake and food 
-RED = (255,0,0) #head
+BLACK = (0, 0, 0)  #background
+WHITE = (255, 255, 255)  #snake and food
+RED = (255, 0, 0)  #head
 
 #intializing snake
+snake = snake.Snake(XLEN, YLEN, WIDTH)
+food = food.Food(XLEN, YLEN)
 
+while True:  #main game loop
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
 
-while True: #main game loop
-	for event in pygame.event.get():
-		if event.type == QUIT:
-			pygame.quit()
-			sys.exit()
-	pygame.display.update()
+        snake.move(event)
+    pygame.display.update()
--- snake.py
+++ snake.py
@@ -1,48 +1,65 @@
 import pygame, sys
 from pygame.locals import *
+import food
+from Queue import *
+import main
 
 #colors
 WHITE = (255,255,255) #snake and food 
 RED = (255,0,0) #head
 
-class snake():
-	def __init__(self, xcoord, ycoord):
-		self.x = xcoord/2
-		self.y = ycoord/2
-		self.width = 20
-		self.head = pygame.Rect(self.x-self.width/2, self.y + self.width/2, self.width, self.width) #makes a square in the center of the screen
-		self.tail = []
-		self.velocity = (0,0)
-		self.pos = (self.x, self.y)
 
-	#returns position of head of snake
-	def get_pos(self):
-		return (self.x, self.y)
+class Snake:
+    def __init__(self, xcoord, ycoord, width):
+        self.x = (xcoord - self.width)/2
+        self.y = (ycoord + self.width)/2 #these two are useless...atm.
+        self.width = width
+        # makes a square in the center of the screen
+        self.head = pygame.Rect(self.x, self.y, self.width, self.width)
+        self.tail = []
+        self.velocity = (0, 0)
+        self.pos = (self.x, self.y)
 
-	#returns velocity
-	def get_velocity(self):
-		return self.velocity
+    #returns position of head of snake
+    def get_pos(self):
+        return self.head.center
 
-	#determines direction the snake moves
-	def move(self, action):
-		# if action == pygame.K_LEFT:
-		# 	self.head.move((-self.velocity,0))
-		# elif action == pygame.K_RIGHT:
-		# 	self.head.move((self.velocity,0))
-		# elif action == pygame.K_UP:
-		# 	self.head.move((0,-self.velocity))
-		# elif action == pygame.K_DOWN:
-		# 	self.head.move((0,self.velocity))
-		if action == pygame.K_LEFT:
-			self.velocity = (-self.width,0)
-		elif action == pygame.K_RIGHT:
-			self.velocity = (self.width,0)
-		elif action == pygame.K_UP:
-			self.velocity = (0, -self.width)
-		elif action == pygame.K_DOWN:
-			self.velocity = (0, self.width)		
+    #returns velocity
+    def get_velocity(self):
+        return self.velocity
 
-	#updates the snake's positions. 
-	def update(self, food_pos):
-		if self.head.collidepoint(food_pos):
-			self.append.tail()
+    #determines direction the snake moves based on user input.
+    #do not let user move 180 from current velocity.
+    def move(self, action):
+        # if action == pygame.K_LEFT:
+        #   self.head.move((-self.velocity,0))
+        # elif action == pygame.K_RIGHT:
+        #   self.head.move((self.velocity,0))
+        # elif action == pygame.K_UP:
+        #   self.head.move((0,-self.velocity))
+        # elif action == pygame.K_DOWN:
+        #   self.head.move((0,self.velocity))
+        if action == pygame.K_LEFT and self.velocity != (self.width, 0):
+            self.velocity = (-self.width, 0)
+        elif action == pygame.K_RIGHT and self.velocity != (-self.width, 0):
+            self.velocity = (self.width, 0)
+        elif action == pygame.K_UP and self.velocity != (0, self.width):
+            self.velocity = (0, -self.width)
+        elif action == pygame.K_DOWN  and self.velocity != (0, -self.width):
+            self.velocity = (0, self.width)
+
+    #updates the snake's positions. Returns -1 if the snake dies. Otherwise returns True if the snake ate the food.
+    def update(self, food):
+        self.head.move(self.velocity)
+        #self.head.move(self.velocity[0], self.velocity[1])
+        for segment in self.tail:
+            segment.move(self.velocity)
+            #segment.move(self.velocity[0], self.velocity[1])
+        if self.head.collidelist(self.tail) or self.x >= main.XLEN :
+            return -1
+        ate_food = self.head.collidepoint(food.get_pos())
+        new_tail_rect = pygame.Rect(self.get_pos(), (self.width, self.width))
+        self.tail.append(new_tail_rect)
+        if not ate_food:
+            self.tail.get()
+        return ate_food
